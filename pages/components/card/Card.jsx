4c6c38660171402a02644da7d653f2ae8f2d7e51
SSahil <ssasahil000@gmail.com>
2022-01-15 10:29:27 +0530

    added-questions

--- pages/components/card/Card.jsx
+++ pages/components/card/Card.jsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../../../styles/Card.module.css";
 import Questions from "./Questions";
 
+import questions from "../data/questions.json";
+
 const Card = () => {
   const [questionCard, setQuestionCard] = useState(false);
+  const [questionIndex, setQuestionIndex] = useState(0);
+
+  const [clicked, setClicked] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleNext = () => {
+    if (questionIndex < questions.length - 1) {
+      setQuestionIndex(questionIndex + 1);
+    } else {
+      // show score
+    }
+
+    setMessage("");
+    setClicked(false);
+  };
 
   return (
     <div className={styles.card}>
@@ -17,7 +34,29 @@ const Card = () => {
           </button>
         </>
       ) : (
-        <Questions />
+        <>
+          <Questions
+            questions={questions}
+            question={questions[questionIndex]}
+            questionIndex={questionIndex}
+            setQuestionIndex={setQuestionIndex}
+            setMessage={setMessage}
+            clicked={clicked}
+            setClicked={setClicked}
+          />
+
+          {clicked && (
+            <>
+              <hr style={{ color: "#ccc" }} />
+              <div className={styles.footer}>
+                <p className={styles.message}>{message}</p>
+                <button className={styles.card_btn} onClick={handleNext}>
+                  {questionIndex !== questions.length - 1 ? "Next" : "Finish"}
+                </button>
+              </div>
+            </>
+          )}
+        </>
       )}
     </div>
   );
--- pages/components/card/Questions.jsx
+++ pages/components/card/Questions.jsx
@@ -1,7 +1,38 @@
-const Questions = () => {
+import { useState } from "react";
+import styles from "../../../styles/Card.module.css";
+
+const Questions = ({
+  questions,
+  question,
+  questionIndex,
+  setQuestionIndex,
+  setMessage,
+  clicked,
+  setClicked,
+}) => {
+  const [correctAnswer, setCorrectAnswer] = useState(false);
+
+  const answerHandler = option => {
+    if (option === question.answer) setMessage("Correct!");
+    else setMessage("Incorrect!");
+
+    setClicked(true);
+  };
+
   return (
     <>
-      <h1>questions here</h1>
+      <h2>{question.questionText}</h2>
+
+      {question.options.map((option, index) => (
+        <button
+          key={index}
+          className={`${styles.option_btn} ${clicked ? styles.option_clicked : ""}`}
+          onClick={() => answerHandler(option)}
+          disabled={clicked}
+        >
+          {option}
+        </button>
+      ))}
     </>
   );
 };
